56ca400a8ffa07748d845407aece5389bf191cd6
gferragu <gferragu@uoregon.com>
2020-08-01 19:38:37 -0700

    Adding SQL lite database

--- task_master_tutorial/app.py
+++ task_master_tutorial/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, url_for
-from flask.sqlalchemy import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy # Note, the 'SQAL' portion ***needs*** to be capitalized
 from datetime import datetime
+
+# To run in new terminal:
+#/opt/anaconda3/envs/flask/bin/python /Users/gabriel/Documents/Web_dev/Flask/task_master_tutorial/app.py
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
-# db = sqlalchemy(app)
 
 class to_do(db.Model):
     id = db.Column(db.Integer, primary_key=True)
